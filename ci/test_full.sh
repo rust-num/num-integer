8cc5deaa59dd73ba176b898ea03396af44c26a70
Josh Stone <cuviper@gmail.com>
2018-06-09 01:30:25 -0700

    Add a trait Roots with sqrt, cbrt, and nth_root

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -17,3 +17,7 @@ if [[ "$TRAVIS_RUST_VERSION" =~ ^(nightly|beta|stable)$ ]]; then
     cargo build --verbose --features=i128
     cargo test --verbose --features=i128
 fi
+
+if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+    cargo test --verbose --all-features --benches
+fi
