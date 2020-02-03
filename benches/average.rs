7581fcb162ff20d28cdb5ad3f0655c158c02c2ec
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-02-03 12:30:26 +0100

    Refactor `benches/average.rs` and check results of methods that should be exact

--- benches/average.rs
+++ benches/average.rs
@@ -6,35 +6,17 @@ extern crate num_integer;
 extern crate num_traits;
 extern crate test;
 
+use std::cmp::{min, max};
+use std::fmt::Debug;
 use num_integer::Integer;
 use num_traits::{AsPrimitive, PrimInt, WrappingAdd, WrappingMul};
 use test::{black_box, Bencher};
 
+// --- Utilities for RNG ----------------------------------------------------
+
 trait BenchInteger: Integer + PrimInt + WrappingAdd + WrappingMul + 'static {}
 
 impl<T> BenchInteger for T where T: Integer + PrimInt + WrappingAdd + WrappingMul + 'static {}
-
-trait NaiveAverage {
-    fn naive_average_floor(&self, other: &Self) -> Self;
-    fn naive_average_ceil(&self, other: &Self) -> Self;
-}
-
-trait UncheckedAverage {
-    fn unchecked_average_floor(&self, other: &Self) -> Self;
-    fn unchecked_average_ceil(&self, other: &Self) -> Self;
-}
-
-fn bench<T, F>(b: &mut Bencher, v: &[(T, T)], f: F)
-where
-    T: Integer,
-    F: Fn(&T, &T) -> T,
-{
-    b.iter(|| {
-        for (x, y) in v {
-            black_box(f(x, y));
-        }
-    });
-}
 
 // Simple PRNG so we don't have to worry about rand compatibility
 fn lcg<T>(x: T) -> T
@@ -49,26 +31,43 @@ where
     x.wrapping_mul(&LCG_A.as_()).wrapping_add(&LCG_C.as_())
 }
 
+// --- Alt. Implementations -------------------------------------------------
+
+trait NaiveAverage {
+    fn naive_average_ceil(&self, other: &Self) -> Self;
+    fn naive_average_floor(&self, other: &Self) -> Self;
+}
+
+trait UncheckedAverage {
+    fn unchecked_average_ceil(&self, other: &Self) -> Self;
+    fn unchecked_average_floor(&self, other: &Self) -> Self;
+}
+
+trait ModuloAverage {
+    fn modulo_average_ceil(&self, other: &Self) -> Self;
+    fn modulo_average_floor(&self, other: &Self) -> Self;
+}
+
 macro_rules! naive_average {
     ($T:ident) => {
         impl super::NaiveAverage for $T {
             fn naive_average_floor(&self, other: &$T) -> $T {
                 match self.checked_add(*other) {
-                    Some(z) => z / 2,
+                    Some(z) => z.div_floor(&2),
                     None => {
                         if self > other {
                             let diff = self - other;
-                            other + diff / 2
+                            other + diff.div_floor(&2)
                         } else {
                             let diff = other - self;
-                            self + diff / 2
+                            self + diff.div_floor(&2)
                         }
                     }
                 }
             }
             fn naive_average_ceil(&self, other: &$T) -> $T {
                 match self.checked_add(*other).and_then(|x| x.checked_add(1)) {
-                    Some(z) => z / 2,
+                    Some(z) => z.div_ceil(&2),
                     None => {
                         if self > other {
                             let diff = self - other;
@@ -97,16 +96,95 @@ macro_rules! unchecked_average {
     };
 }
 
+macro_rules! modulo_average {
+    ($T:ident) => {
+        impl super::ModuloAverage for $T {
+            fn modulo_average_ceil(&self, other: &$T) -> $T {
+                let (q1, r1) = self.div_mod_floor(&2);
+                let (q2, r2) = other.div_mod_floor(&2);
+                q1 + q2 + (r1 * r2)
+            }
+            fn modulo_average_floor(&self, other: &$T) -> $T {
+                let (q1, r1) = self.div_mod_floor(&2);
+                let (q2, r2) = other.div_mod_floor(&2);
+                q1 + q2 + (r1 * r2)
+            }
+        }
+    };
+}
+
+// --- Bench functions ------------------------------------------------------
+
+fn bench_unchecked<T, F>(b: &mut Bencher, v: &[(T, T)], f: F)
+where
+    T: Integer + Debug + Copy,
+    F: Fn(&T, &T) -> T,
+{
+    b.iter(|| {
+        for (x, y) in v {
+            black_box(f(x, y));
+        }
+    });
+}
+
+fn bench_ceil<T, F>(b: &mut Bencher, v: &[(T, T)], f: F)
+where
+    T: Integer + Debug + Copy,
+    F: Fn(&T, &T) -> T,
+{
+    for &(i, j) in v {
+        let rt = f(&i, &j);
+        let (a, b) = (min(i, j), max(i, j));
+        if (b - a).is_even() {
+            assert_eq!(rt - a, b - rt);
+        } else {
+            assert_eq!(rt - a, b - rt + T::one());
+        }
+    }
+    bench_unchecked(b, v, f);
+}
+
+fn bench_floor<T, F>(b: &mut Bencher, v: &[(T, T)], f: F)
+where
+    T: Integer + Debug + Copy,
+    F: Fn(&T, &T) -> T,
+{
+    for &(i, j) in v {
+        let rt = f(&i, &j);
+        let (a, b) = (min(i, j), max(i, j));
+        println!("{:?} + {:?} / 2 = {:?}", a, b, rt);
+        // if both number are the same sign, check rt is in the middle
+        if (a < T::zero()) == (b < T::zero()) {
+            if (b - a).is_even() {
+                assert_eq!(rt - a, b - rt);
+            } else {
+                assert_eq!(rt - a + T::one(), b - rt);
+            }
+        // if both number have a different sign,
+        } else {
+            if (a + b).is_even() {
+                assert_eq!(rt, (a + b) / (T::one() + T::one()))
+            } else {
+                assert_eq!(rt, (a + b - T::one()) / (T::one() + T::one()))
+            }
+        }
+    }
+    bench_unchecked(b, v, f);
+}
+
+// --- Bench implementation -------------------------------------------------
+
 macro_rules! bench_average {
     ($($T:ident),*) => {$(
         mod $T {
             use test::Bencher;
-            use num_integer::Average;
-            use UncheckedAverage;
-            use NaiveAverage;
+            use num_integer::{Average, Integer};
+            use super::{UncheckedAverage, NaiveAverage, ModuloAverage};
+            use super::{bench_ceil, bench_floor, bench_unchecked};
 
             naive_average!($T);
             unchecked_average!($T);
+            modulo_average!($T);
 
             const SIZE: $T = 30;
 
@@ -133,59 +211,99 @@ macro_rules! bench_average {
                     .collect()
             }
 
-            #[bench]
-            fn average_floor_small(b: &mut Bencher) {
-                let v = small();
-                super::bench(b, &v, |x: &$T, y: &$T| x.average_floor(y));
+            mod floor {
+
+                use super::*;
+
+                mod small {
+
+                    use super::*;
+
+                    #[bench]
+                    fn optimized(b: &mut Bencher) {
+                        let v = small();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.average_floor(y));
+                    }
+
+                    #[bench]
+                    fn naive(b: &mut Bencher) {
+                        let v = small();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn unchecked(b: &mut Bencher) {
+                        let v = small();
+                        bench_unchecked(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn modulo(b: &mut Bencher) {
+                        let v = small();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.modulo_average_floor(y));
+                    }
+                }
+
+                mod overflowing {
+
+                    use super::*;
+
+                    #[bench]
+                    fn optimized(b: &mut Bencher) {
+                        let v = overflowing();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.average_floor(y));
+                    }
+
+                    #[bench]
+                    fn naive(b: &mut Bencher) {
+                        let v = overflowing();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn unchecked(b: &mut Bencher) {
+                        let v = overflowing();
+                        bench_unchecked(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn modulo(b: &mut Bencher) {
+                        let v = overflowing();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.modulo_average_floor(y));
+                    }
+                }
+
+                mod rand {
+
+                    use super::*;
+
+                    #[bench]
+                    fn optimized(b: &mut Bencher) {
+                        let v = rand();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.average_floor(y));
+                    }
+
+                    #[bench]
+                    fn naive(b: &mut Bencher) {
+                        let v = rand();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn unchecked(b: &mut Bencher) {
+                        let v = rand();
+                        bench_unchecked(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
+                    }
+
+                    #[bench]
+                    fn modulo(b: &mut Bencher) {
+                        let v = rand();
+                        bench_floor(b, &v, |x: &$T, y: &$T| x.modulo_average_floor(y));
+                    }
+                }
+
             }
 
-            #[bench]
-            fn average_floor_small_naive(b: &mut Bencher) {
-                let v = small();
-                super::bench(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_small_unchecked(b: &mut Bencher) {
-                let v = small();
-                super::bench(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_overflowing(b: &mut Bencher) {
-                let v = overflowing();
-                super::bench(b, &v, |x: &$T, y: &$T| x.average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_overflowing_naive(b: &mut Bencher) {
-                let v = overflowing();
-                super::bench(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_overflowing_unchecked(b: &mut Bencher) {
-                let v = overflowing();
-                super::bench(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_rand(b: &mut Bencher) {
-                let v = rand();
-                super::bench(b, &v, |x: &$T, y: &$T| x.average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_rand_naive(b: &mut Bencher) {
-                let v = rand();
-                super::bench(b, &v, |x: &$T, y: &$T| x.naive_average_floor(y));
-            }
-
-            #[bench]
-            fn average_floor_rand_unchecked(b: &mut Bencher) {
-                let v = rand();
-                super::bench(b, &v, |x: &$T, y: &$T| x.unchecked_average_floor(y));
-            }
         }
     )*}
 }
