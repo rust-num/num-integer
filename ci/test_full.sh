cba0dac49d75ba7d9ea5fca2b8eb4223fd40abd8
Josh Stone <cuviper@gmail.com>
2020-04-09 21:33:27 -0700

    Switch CI to GitHub Actions

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-echo Testing num-integer on rustc ${TRAVIS_RUST_VERSION}
+CRATE=num-integer
+MSRV=1.8
 
-# num-integer should build and test everywhere.
-cargo build --verbose
-cargo test --verbose
+get_rust_version() {
+  local array=($(rustc --version));
+  echo "${array[1]}";
+  return 0;
+}
+RUST_VERSION=$(get_rust_version)
 
-# test `no_std`
-cargo build --verbose --no-default-features
-cargo test --verbose --no-default-features
+check_version() {
+  IFS=. read -ra rust <<< "$RUST_VERSION"
+  IFS=. read -ra want <<< "$1"
+  [[ "${rust[0]}" -gt "${want[0]}" ||
+   ( "${rust[0]}" -eq "${want[0]}" &&
+     "${rust[1]}" -ge "${want[1]}" )
+  ]]
+}
 
-# test `i128`
-if [[ "$TRAVIS_RUST_VERSION" =~ ^(nightly|beta|stable)$ ]]; then
-    cargo build --verbose --features=i128
-    cargo test --verbose --features=i128
+echo "Testing $CRATE on rustc $RUST_VERSION"
+if ! check_version $MSRV ; then
+  echo "The minimum for $CRATE is rustc $MSRV"
+  exit 1
 fi
 
-if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
-    cargo test --verbose --all-features --benches
+FEATURES=()
+check_version 1.26 && FEATURES+=(i128)
+echo "Testing supported features: ${FEATURES[*]}"
+
+set -x
+
+# test the default
+cargo build
+cargo test
+
+# test `no_std`
+cargo build --no-default-features
+cargo test --no-default-features
+
+# test each isolated feature, with and without std
+for feature in ${FEATURES[*]}; do
+  cargo build --no-default-features --features="std $feature"
+  cargo test --no-default-features --features="std $feature"
+
+  cargo build --no-default-features --features="$feature"
+  cargo test --no-default-features --features="$feature"
+done
+
+# test all supported features, with and without std
+cargo build --features="std ${FEATURES[*]}"
+cargo test --features="std ${FEATURES[*]}"
+
+cargo build --features="${FEATURES[*]}"
+cargo test --features="${FEATURES[*]}"
+
+if rustc --version | grep -q nightly; then
+    cargo test --all-features --benches
 fi
